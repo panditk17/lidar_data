ef12160118e2d0afadb12801e5500714b4fc07f7
Pandit <karunpandit@ufl.edu>
2022-02-12 22:20:44 -0500

    code_update

--- lidar_script_1.R
+++ lidar_script_1.R
@@ -15,6 +15,18 @@ xyz<-subset(LAS[,1:3],LAS[,3] >= 1.37)
 set.seed(1)
 clLAS<-kmeans(xyz, 32)
 
+
+ppp2<-kmeans(ppp,16)
+
+id<-as.factor(ppp2$cluster)
+ppp4<-cbind(ppp[,1:2],id)
+chullTrees<-chullLiDAR2D(ppp4)
+
+plot(SpatialPoints(ppp[,1:2]),cex=0.5,col=ppp[,3])
+plot(chullTrees$chullPolygon,add=TRUE, border='green')
+
+
+
 # Set the points id
 id<-as.factor(clLAS$cluster)
 
@@ -51,6 +63,11 @@ xyzid<-cbind(xyz,id)
 library(rgl)
 open3d()
 
+plot3d()
+
+volumeList<-chullLiDAR3D(xyzid=ppp, plotit=plotit, col=col,alpha=alpha)
+
+
 volumeList<-chullLiDAR3D(xyzid=xyzid, plotit=plotit, col=col,alpha=alpha)
 summary(volumeList) # summary
 
@@ -60,6 +77,33 @@ grid3d(side=c('x+', 'y-', 'z'), col="gray") # grid
 title3d(xlab = "UTM Easthing", ylab = "UTM Northing",zlab = "Height", col="red")
 aspect3d(1,1,0.7) # scale
 
-
-
+library(raster)
+jjj<-raster("OSBS_chm.tif")
+plot(jjj)
+data(chm)  
+  
+  
+  # Importing the LiDAR-derived CHM file
+  data(chm) # or set a CHM. e.g. chm<-raster("CHM_stand.asc")
+#=======================================================================#
+# Example 01: Smoothing the CHM using a Gaussian filter
+#=======================================================================#
+# Set the ws:
+ws<-3 # dimension 3x3
+# Set the filter type
+filter<-"Gaussian"
+# Set the sigma value
+sigma<-0.6
+# Smoothing CHM
+sCHM<-CHMsmoothing(jjj, filter, ws, sigma)
+#=======================================================================#
+# Example 02: Smoothing the CHM using a mean filter
+#=======================================================================#
+# Set the ws:
+ws<-5 # dimension 5x5
+# Set the filter type
+filter<-"mean"
+# Smoothing and plotting LiDAR-derived CHM
+sCHM<-CHMsmoothing(jjj, filter, ws)
+plot(sCHM)
 
---
+++ lidar_script_2.R
@@ -0,0 +1,115 @@
+
+## script to read liar data and plot
+
+library(rLiDAR)
+
+LASfile <- system.file("extdata", "LASexample1.las", package="rLiDAR")
+
+# Read LAS file
+LAS<-readLAS(LASfile,short=TRUE)
+
+library(raster)
+
+las2<-raster("../lid_data/OSBS_chm.tif")
+plot(las2)
+
+extent<-c(xmin=404000,xmax=404050,ymin=3285000,ymax=3285050)
+
+las3<-crop(las2,extent)
+plot(las3)
+
+xyz<-rasterToPoints(las3)
+
+# subset the data for height
+# xyz<-subset(LAS[,1:3],LAS[,3] >= 1.37)
+
+# get LiDAR clusters
+set.seed(1)
+clLAS<-kmeans(xyz, 100)
+
+# Set the points id
+id<-as.factor(clLAS$cluster)
+
+# Set the xyid input
+xyid<-cbind(xyz[,1:2],id)
+
+# Compute the LiDAR convex hull of the clusters
+chullTrees<-chullLiDAR2D(xyid)
+
+# Plotting the LiDAR convex hull
+library(sp)
+plot(SpatialPoints(xyid[,1:2]),cex=0.5,col=xyid[,3])
+plot(chullTrees$chullPolygon,add=TRUE, border='green')
+
+# Get the ground-projected area of LiDAR convex hull
+chullList<-chullTrees$chullArea
+summary(chullList) # summary
+
+### plot 3D of lidar convex hull
+
+# Set the alpha
+alpha<-0.3
+
+# Set the plotCAS parameter
+plotit=TRUE
+
+# Set the convex hull color
+col="forestgreen"
+
+# Combining xyz and id
+xyzid<-cbind(xyz,id)
+
+# Get the volume and surface area
+library(rgl)
+open3d()
+
+# plot3d()
+
+# volumeList<-chullLiDAR3D(xyzid=ppp, plotit=plotit, col=col,alpha=alpha)
+
+
+volumeList<-chullLiDAR3D(xyzid=xyzid, plotit=plotit, col=col,alpha=alpha)
+summary(volumeList) # summary
+
+plot3d(xyzid[,1:3], add=TRUE) # add the 3D point cloud
+axes3d(c("x+", "y-", "z-")) # axes
+grid3d(side=c('x+', 'y-', 'z'), col="gray") # grid
+title3d(xlab = "UTM Easthing", ylab = "UTM Northing",zlab = "Height", col="red")
+aspect3d(1,1,0.7) # scale
+
+
+sCHM<-CHMsmoothing(las3, filter="mean", ws=5) # smoothing CHM
+loc<-FindTreesCHM(sCHM, fws=5, minht=8) # or import a tree list
+# Set the maxcrown parameter
+maxcrown=10.0
+# Set the exclusion parameter
+exclusion=0.3 # 30
+# Compute individual tree detection canopy area
+canopy<-ForestCAS(las3, loc, maxcrown, exclusion)
+
+boundaryTrees<-canopy[[1]]
+# Plotting the individual tree canopy boundary over the CHM
+plot(las3, main="LiDAR-derived CHM")
+# adding tree canopy boundary
+plot(boundaryTrees, add = TRUE, border = 'red', bg = 'transparent')
+
+
+
+#============================================================================#
+# Retrieving the list of individual trees detected for canopy area calculation
+#============================================================================#
+canopyList<-canopy[[2]] # list of ground-projected areas of individual tree canopies
+summary(canopyList) # summary
+# Spatial location of the trees
+library(sp)
+XY<-SpatialPoints(canopyList[,1:2]) # Spatial points
+plot(XY, col = "black", add = TRUE, pch = "*") # adding tree location to the plot
+
+
+
+
+LiDARForestStand(crownshape = c( "cone"), CL = 14, CW = 8, HCB = 10,
+                 X = 0, Y = 0, dbh = 0.3, crowncolor = "forestgreen",
+                 stemcolor = "chocolate4", resolution="high", mesh=TRUE)
+
+
