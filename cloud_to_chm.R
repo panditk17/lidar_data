15800fc618f179ae1b5f53f21bb81d49af61ccd7
Karun Pandit <karunpandit@gmail.com>
2024-12-31 04:51:12 -0500

    Update cloud_to_chm.R

--- cloud_to_chm.R
+++ cloud_to_chm.R
@@ -10,25 +10,30 @@ hist(las$Z)
 col <- height.colors(50)
 
 # Points-to-raster algorithm with a resolution of 1 meter
+# canopy height 
 chm <- grid_canopy(las, res = 1, p2r())
 plot(chm, col = col)
 
 ## create terrain model with las file
 dtm = grid_terrain(las, algorithm = tin())
-
 plot(dtm)
 
+# canopy height minus terrain
 chm2<-chm-dtm
 plot(chm2)
 
 writeRaster(chm,"../lid_data/clipped_chm.tif",overwrite=TRUE)
 # Points-to-raster algorithm with a resolution of 0.5 meters replacing each
+
+## Try alternate models
 # point by a 20-cm radius circle of 8 points
 chm <- grid_canopy(las, res = 0.5, p2r(0.2))
 plot(chm, col = col)
+
 # Basic triangulation and rasterization of first returns
 chm <- grid_canopy(las, res = 0.5, dsmtin())
 plot(chm, col = col)
+
 # Khosravipour et al. pitfree algorithm
 chm <- grid_canopy(las, res = 0.5, pitfree(c(0,2,5,10,15), c(0, 1.5)))
 plot(chm, col = col)
